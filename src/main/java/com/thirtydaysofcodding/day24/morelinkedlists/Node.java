ab75af0da177f32fb8b97fa125f10337bc25ebd0
Aldo Bertero <abertero@antica.com>
2017-02-24 08:52:37 -0300

    New challenges solved

---
+++ src/main/java/com/thirtydaysofcodding/day24/morelinkedlists/Node.java
@@ -0,0 +1,12 @@
+package com.thirtydaysofcodding.day24.morelinkedlists;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+
+}
