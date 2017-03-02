c40820b74450424aeec45c182c08e74fae159beb
Aldo Bertero <abertero@antica.com>
2017-03-02 14:44:17 -0300

    New incomplete challenge

--- src/main/java/com/algorithms/dynamicprogramming/TheMaximumSubarray.java
+++ src/main/java/com/algorithms/dynamicprogramming/TheMaximumSubarray.java
@@ -11,41 +11,72 @@ public class TheMaximumSubarray {
         int t = scn.nextInt();
         for (int i = 0; i < t; i++) {
             int n = scn.nextInt();
-            int[] arr = new int[n];
+            long[] arr = new long[n];
             for (int j = 0; j < n; j++) {
-                arr[j] = scn.nextInt();
+                arr[j] = scn.nextLong();
             }
-            Map.Entry<Integer, Integer> result = getMaximumSubarray(arr, n);
+            Map.Entry<Long, Long> result = getMaximumSubarray(arr, n);
             System.out.println(String.format("%s %s", result.getKey(), result.getValue()));
         }
         scn.close();
     }
 
-    private static Map.Entry<Integer, Integer> getMaximumSubarray(final int[] arr, final int n) {
-        int discont = Integer.MIN_VALUE;
-        int sumOfEverything = 0;
+    private static Map.Entry<Long, Long> getMaximumSubarray(long[] arr, int n) {
+        long discont = Integer.MIN_VALUE;
+        int contI = 0;
+        int contJ = 0;
+        long maxCont = Integer.MIN_VALUE;
+        long newPossibleCont = -1;
+        int newPosI = -1;
+        int newPosJ = -1;
+
         for (int i = 0; i < n; i++) {
-            if (arr[i] > 0 || arr[i] > discont) {
+            if (arr[i] < 0 && arr[i] > discont) {
+                discont = arr[i];
+                maxCont = arr[i];
+                contI = 0;
+                contJ = 0;
+            } else if (arr[i] > 0) {
+                if (discont < 0) {
+                    discont = 0;
+                }
+                if (maxCont < 0) {
+                    maxCont = arr[i];
+                    contI = 0;
+                    contJ = 0;
+                } else if (contJ == i - 1) {
+                    maxCont += arr[i];
+                    contJ = i;
+                } else if (newPosJ == i - 1) {
+                    newPossibleCont += arr[i];
+                    newPosJ = i;
+                    if (newPossibleCont > maxCont) {
+                        maxCont = newPossibleCont;
+                        contI = newPosI;
+                        contJ = newPosJ;
+                    }
+                } else {
+                    newPossibleCont = arr[i];
+                    newPosI = i;
+                    newPosJ = i;
+                    if (newPossibleCont > maxCont) {
+                        maxCont = newPossibleCont;
+                        contI = newPosI;
+                        contJ = newPosJ;
+                    }
+                }
                 discont += arr[i];
-            }
-            sumOfEverything += arr[i];
-        }
-        int i = 0;
-        int j = n - 1;
-        int contMax = sumOfEverything;
-        int sumOfI = 0;
-        int sumOfJ = 0;
-        while (i < j) {
-            if (contMax - arr[i] > contMax) {
-                contMax = contMax - arr[i];
-                ++i;
-            }
-            if (i < j && contMax - arr[j] > contMax) {
-                contMax = contMax - arr[j];
-                --j;
             }
         }
 
-        return new AbstractMap.SimpleEntry<>(0, discont);
+        return new AbstractMap.SimpleEntry<>(getSum(arr, contI, contJ), discont);
+    }
+
+    private static long getSum(long[] arr, int i, int j) {
+        long result = 0;
+        for (int k = i; k <= j; k++) {
+            result += arr[k];
+        }
+        return result;
     }
 }
