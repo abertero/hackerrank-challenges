7724bb2c22489d4c66d0c5fd198452ca0c3858f6
Aldo Bertero <abertero@antica.com>
2017-03-03 16:35:10 -0300

    New challenge

--- src/main/java/com/algorithms/dynamicprogramming/TheMaximumSubarray.java
+++ src/main/java/com/algorithms/dynamicprogramming/TheMaximumSubarray.java
@@ -23,60 +23,28 @@ public class TheMaximumSubarray {
 
     private static Map.Entry<Long, Long> getMaximumSubarray(long[] arr, int n) {
         long discont = Integer.MIN_VALUE;
-        int contI = 0;
-        int contJ = 0;
-        long maxCont = Integer.MIN_VALUE;
-        long newPossibleCont = -1;
-        int newPosI = -1;
-        int newPosJ = -1;
+        long maxCont = Long.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
             if (arr[i] < 0 && arr[i] > discont) {
                 discont = arr[i];
-                maxCont = arr[i];
-                contI = 0;
-                contJ = 0;
             } else if (arr[i] > 0) {
                 if (discont < 0) {
                     discont = 0;
                 }
-                if (maxCont < 0) {
-                    maxCont = arr[i];
-                    contI = 0;
-                    contJ = 0;
-                } else if (contJ == i - 1) {
-                    maxCont += arr[i];
-                    contJ = i;
-                } else if (newPosJ == i - 1) {
-                    newPossibleCont += arr[i];
-                    newPosJ = i;
-                    if (newPossibleCont > maxCont) {
-                        maxCont = newPossibleCont;
-                        contI = newPosI;
-                        contJ = newPosJ;
-                    }
-                } else {
-                    newPossibleCont = arr[i];
-                    newPosI = i;
-                    newPosJ = i;
-                    if (newPossibleCont > maxCont) {
-                        maxCont = newPossibleCont;
-                        contI = newPosI;
-                        contJ = newPosJ;
-                    }
-                }
                 discont += arr[i];
+            }
+            for (int j = i; j < n; j++) {
+                long auxSum = 0;
+                for (int k = i; k <= j; k++) {
+                    auxSum += arr[k];
+                }
+                if (auxSum > maxCont) {
+                    maxCont = auxSum;
+                }
             }
         }
 
-        return new AbstractMap.SimpleEntry<>(getSum(arr, contI, contJ), discont);
-    }
-
-    private static long getSum(long[] arr, int i, int j) {
-        long result = 0;
-        for (int k = i; k <= j; k++) {
-            result += arr[k];
-        }
-        return result;
+        return new AbstractMap.SimpleEntry<>(maxCont, discont);
     }
 }
